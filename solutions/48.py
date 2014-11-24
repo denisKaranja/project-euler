7f8ce231cb6a0eafb680c869035628126347a10d
Denis Karanja <deebeat@students.uonbi.ac.ke>
2014-11-25 00:36:34 +0300

    Solved problem 22(22.py) and problem 48(48.py)

--- solutions/48.py
+++ solutions/48.py
@@ -2,13 +2,12 @@
 #School of Computing and Informatics,
 #The University of Nairobi,
 #dee.caranja@gmail.com
-#Euler project solution = 48(Self powers)
+#Euler project solution = 48(Sum of Self powers i.e 1^1 + 2^2 + 3^3 +...+ 100^100)
 
 def selfPower(num):
 	if num == 1:
 		return 1
 	else:
-		
-		return num + selfPower(num - 1)
+		return num**num + selfPower(num - 1)
 
-print selfPower(10)
+print selfPower(100)
