f385b9ee8161dbcef6d076e2928e1b78a1604ecb
Denis Karanja <deebeat@students.uonbi.ac.ke>
2014-12-06 00:06:09 +0300

    More core dump fix in 3.py

--- solutions/3.py
+++ solutions/3.py
@@ -22,24 +22,23 @@ def prime(num):
 	
 	return True
 def max(list):
-	for i in range(len(list)):
-		max = list[i]
-		if list[i] > max:
-			list[i] = max
-	return max
+	for i in xrange(len(list)):
+		maxNum = list[i]
+		if list[i] > maxNum:
+			list[i] = maxNum
+	return maxNum
 
-limit = 2000
-number = 600851475143
+number = 13195
 output = []
 i = 0
 while i <= number:
-	if prime(i) is True:
+	if prime(i):
 		if number % i == 0:
 			output.append(i)
 			print "Prime num found->> {}".format(i)
 	i += 1
-print max(output)
+print "\nLargest prime factor for %d is %d" % (number, max(output))
 print "Program took %.4f secs to execute\n"%(time.clock() - startTime)
 
-
+numberCopy = 600851475143
 
