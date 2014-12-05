26eecb4c07088f698621d1ea3364268d6d34e0b6
Denis Karanja <deebeat@students.uonbi.ac.ke>
2014-12-05 19:41:50 +0300

    Problem completely solved using iterrative fibonacci method

--- solutions/2.py
+++ solutions/2.py
@@ -1,8 +1,11 @@
-#Author: Denis Karanja,
-#Institution: The University of Nairobi, Kenya,
-#Department: School of Computing and Informatics,
-#Email: dee.caranja@gmail.com,
-#Euler project solution = 2(Even fibonacci number <= 4,000,000)
+"""
+Author: Denis Karanja,
+Institution: The University of Nairobi, Kenya,
+Department: School of Computing and Informatics,
+Email: dee.caranja@gmail.com,
+Euler project solution = 2(Even fibonacci number <= 4,000,000)
+Status:..COMPELETED
+"""
 
 #itterative Fibo
 def iterrativeFibo(num):
