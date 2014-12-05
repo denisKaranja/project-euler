9e22393b333834fef0eb45018d5bbbf512f635f8
Denis Karanja <deebeat@students.uonbi.ac.ke>
2014-12-05 19:40:34 +0300

    Problem completely solved:)

--- solutions/10.py
+++ solutions/10.py
@@ -1,27 +1,26 @@
-#Author: Denis Karanja,
-#Institution: The University of Nairobi, Kenya,
-#Department: School of Computing and Informatics,
-#Email: dee.caranja@gmail.com,
-#Euler project solution = 10(Sum of Prime Numbers)
-sumOfPrime = 0
-number = 0
+"""
+Author: Denis Karanja,
+Institution: The University of Nairobi, Kenya,
+Department: School of Computing and Informatics,
+Email: dee.caranja@gmail.com,
+Euler project solution = 10(Sum of Prime Numbers)
+Status:...COMPLETED
 
-for i in range(2, 10000):
-	if (i % 1 == 0) and (i % i == 0) and (i % 2 is not 0):
-		if(i < 9):	
-			sumOfPrime += i
-			print i
-			number += 1
-		else:
-			if (i % 3 is not 0) and (i % 5 is not 0) and (i % 7 is not 0):
-				sumOfPrime += i
-				print i
-				number += 1
+"""
+import time
+startTime = time.clock()
+print "Calculating..."
+def prime(num):
+	if num < 2: return False
+	if num % 2 == 0: return num == 2
 
-	elif i == 2:
-		print i
-		sumOfPrime += i
-		number += 1
+	division = 3
 
-print sumOfPrime
-print "This is the %dth prime" % number
+	while (division * division) <= num:
+		if num % division == 0: return False
+		division += 2
+	return True
+
+limit = 2000000
+print "sum is ->> {}".format(sum(filter(prime, range(1, limit))))
+print "Program took %.4f secs to execute\n"%(time.clock() - startTime)
