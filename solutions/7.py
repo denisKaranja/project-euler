c0b184956b00665d3845edf9157a9141c2a9f7d7
Denis Karanja <deebeat@students.uonbi.ac.ke>
2014-12-05 19:38:46 +0300

    Problem solved:)

---
+++ solutions/7.py
@@ -0,0 +1,27 @@
+"""
+Author: Denis Karanja,
+Institution: The University of Nairobi, Kenya,
+Department: School of Computing and Informatics,
+Email: dee.caranja@gmail.com,
+Euler project solution = 7(the 10 001st prime number)
+Status:...COMPLETED:)
+"""
+import time
+startTime = time.clock()
+print "Calculating..."
+def prime(num):
+	if num < 2: return False
+	if num % 2 == 0: return num == 2
+
+	division = 3
+
+	while (division * division) <= num:
+		if num % division == 0: return False
+		division += 2
+	return True
+
+limit = 1000000
+length = (filter(prime, range(limit)))
+
+print length[10000]
+print "Program took %.4f secs to execute\n"%(time.clock() - startTime)
