d6efe097e5d42429f01c4ddf651b5f3ad5682a0d
Denis Karanja <deebeat@students.uonbi.ac.ke>
2014-12-06 21:52:26 +0300

    Used pow method in math in 16.py.

--- solutions/16.py
+++ solutions/16.py
@@ -1,14 +1,14 @@
-#Author: Denis Karanja,
-#Institution: The University of Nairobi, Kenya,
-#Department: School of Computing and Informatics,
-#Email: dee.caranja@gmail.com,
-#Euler project solution = 16(sum of digits in 2^1000 eg 2^4 = 16 there4 1+6=7)
-
-def getPow(num, exponent):
-	return num ** exponent
-
+"""
+Author: Denis Karanja,
+Institution: The University of Nairobi, Kenya,
+Department: School of Computing and Informatics,
+Email: dee.caranja@gmail.com,
+Euler project solution = 16(sum of digits in 2^1000 eg 2^4 = 16 there4 1+6=7)
+Status: COMPLETED...
+"""
+import math
 digitSum = 0
-answer = str(getPow(2, 1000))
+answer = str(int(math.pow(2, 1000)))
 
 for digit in answer:
 	digitSum += int(digit)
