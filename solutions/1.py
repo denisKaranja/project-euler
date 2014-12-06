aa2f244ece35df10f7d029a31b93288051157e8f
Denis Karanja <deebeat@students.uonbi.ac.ke>
2014-12-06 21:08:26 +0300

    Code simplified to one line:)

--- solutions/1.py
+++ solutions/1.py
@@ -1,12 +1,16 @@
-#Author: Denis Karanja,
-#Institution: The University of Nairobi, Kenya,
-#Department: School of Computing and Informatics,
-#Email: dee.caranja@gmail.com,
-#Euler project solution = 1(Sum of all numbers divisible by 3 and 5 in range(1000))
-
-sum = 0
+"""
+Author: Denis Karanja,
+Institution: The University of Nairobi, Kenya,
+Department: School of Computing and Informatics,
+Email: dee.caranja@gmail.com,
+Euler project solution = 1(Sum of all numbers divisible by 3 and 5 in range(1000))
+Status: COMPLETED...
+"""
+sumList = 0
 for i in range(1000):
 	if i % 5 == 0 or i % 3 == 0:
-		sum += i
+		sumList += i
+print sumList
 
-print sum
+#One line code
+print sum([x for x in range(1000) if x%3==0 or x%5==0])
