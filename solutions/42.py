21e51298efdfb611ba60dfaa15677bdd0db1ad54
Denis Karanja <deebeat@students.uonbi.ac.ke>
2014-12-10 18:45:07 +0300

    Initial commit

---
+++ solutions/42.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+"""
+Author: Denis Karanja,
+Institution: The University of Nairobi, Kenya,
+Department: School of Computing and Informatics, Chiromo campus
+Email: dee.caranja@gmail.com,
+Task: 42(Sum of Triangle words)
+License type: MIT :)
+
+"""
