5db5308e77b0aa774f756a421e30d96bd78b16d9
Denis Karanja <deebeat@students.uonbi.ac.ke>
2014-12-10 18:50:42 +0300

    Function 'triangle_numbers()' defined to give the triangle sequence

--- solutions/42.py
+++ solutions/42.py
@@ -4,7 +4,9 @@ Author: Denis Karanja,
 Institution: The University of Nairobi, Kenya,
 Department: School of Computing and Informatics, Chiromo campus
 Email: dee.caranja@gmail.com,
-Task: 42(Sum of Triangle words)
+Task: 42(Sum of Triangle words) using the formula ->> Tn = 0.5n(n+1)
 License type: MIT :)
 
 """
+def triangle_numbers(nth_term):
+	return int((0.5 * nth_term)*(nth_term + 1))
