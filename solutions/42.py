305610bf4bcd4e293110c982378d26900d432be7
Denis Karanja <deebeat@students.uonbi.ac.ke>
2014-12-11 17:13:02 +0300

    Problem 42 completed:)

--- solutions/42.py
+++ solutions/42.py
@@ -6,7 +6,7 @@ Department: School of Computing and Informatics, Chiromo campus
 Email: dee.caranja@gmail.com,
 Task: 42(Sum of Triangle words) using the formula ->> Tn = 0.5n(n+1)
 License type: MIT :)
-
+Status: COMPLETED
 """
 import time
 import math
@@ -14,25 +14,31 @@ startTime = time.clock()
 print "\nCalculating triangle word score...\n"
 
 def triangle_numbers(nth_term):
-	return int((0.5 * nth_term)*(nth_term + 1))
+	return ((0.5 * nth_term)*(nth_term + 1))
 
-def quadratic(a=1, b=1, c):
-	inner = b**2 - 4*1*c
-	if inner < 0: return False
+def quadratic(c):
+	#n^2 + n -2sum = 0
+	a = 1
+	b = 1
+	inner = pow(b, 2) - ((4 * a * c) * -2)
+	if inner < 0: 
+		return False
 	else: 
-		inner = math.sqrt(inner)
-		x_one = (-b + inner) / (2*a)
-		return int(x_one)
-quadratic()
+		sq = math.sqrt(inner)
+		x_one = (-b + sq) / (2*a)
+		return x_one
 
-word_list = []
-fake = ["DENIS", "SKY", "COLIN"]
-file_handle = open('p042_words.txt', 'r')
+word_list = ''
+triangle_words = 0
+file_handle = open("p042_words.txt", 'r')
 
 for line in file_handle:
-	word_list.append(line)
+	word_list += line
 
-for index, word in enumerate(fake):
+#strip the new line character(\n)and make the string a list
+new_list = word_list.strip("\n").replace('"', "").split(",")
+
+for index, word in enumerate(new_list):
 	word_sum = 0
 	alphabets = dict(A = 1,B = 2,C = 3,D = 4,E = 5,F = 6,G = 7,H = 8,I = 9,J = 10,K = 11,L = 12,M = 13,
 		N = 14,O = 15,P = 16,Q = 17,R = 18,S = 19,T = 20,U = 21,V = 22,W = 23,X = 24,Y = 25,Z = 26)
@@ -41,9 +47,9 @@ for index, word in enumerate(fake):
 		if alphabets.__contains__(char):
 			word_sum += alphabets.get(char)
 
-	if word_sum == 55:
-		print "\tFound at %d(%s) and is %d" %(index, word, word_sum)
-	else:
-		print "\t%s ->> %s" % (word, str(word_sum))
+	if float(quadratic(word_sum)).is_integer():
+		#print "\tFound at %d(%s) and is %d" %(index, word, word_sum)
+		triangle_words += 1
 
-print "\nProgram took %f secs to execute\n"%(time.clock() - startTime)
+print "\t{} triangle words found bro!".format(triangle_words)
+print "\tRun time... %f secs to execute\n"%(time.clock() - startTime)
