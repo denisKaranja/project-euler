99c3d0af7842f0b10ff37c1d4c74571775a679cf
Denis Karanja <deebeat@students.uonbi.ac.ke>
2014-12-12 11:35:59 +0300

    Initial commit

---
+++ solutions/104.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+"""
+Author: Denis Karanja,
+Institution: The University of Nairobi, Kenya,
+Department: School of Computing and Informatics, Chiromo campus
+Email: dee.caranja@gmail.com,
+Task: 5(A number that is divisible by numbers 1 through 20. eg 2520 is divisible by numbers 1 thru 10)
+License type: MIT :)
+Status: PENDING...
+"""
