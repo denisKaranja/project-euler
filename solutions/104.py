b2900bf99d1e26248a8c4a51a6c25467638b6d84
Denis Karanja <deebeat@students.uonbi.ac.ke>
2014-12-12 13:08:15 +0300

    Revised the recursion fn to an itterative fn to reduce run time

--- solutions/104.py
+++ solutions/104.py
@@ -8,3 +8,32 @@ Task: 5(A number that is divisible by numbers 1 through 20. eg 2520 is divisible
 License type: MIT :)
 Status: PENDING...
 """
+import time
+startTime = time.clock()
+
+"""def recursive_fibo(num):
+	if num == 1 or num == 0:
+		return num
+	else:
+		return recursive_fibo(num - 1) + recursive_fibo(num - 2)"""
+
+def iterrativeFibo(num):
+	x = i = 0
+	y = z = 1
+	for i in range(0, num+1):
+		x = y
+		y = z
+		z = x + y
+	return x
+
+print "Calculating using itterative fibonacci. Please wait..."
+print "\tUsing itterative {}".format(iterrativeFibo(10))
+iterrative_time = time.clock() - startTime
+print "\tRun time... %.5f(secs) or %.5f(mins)" % (iterrative_time, (iterrative_time / 60.0))
+
+"""
+print "\nCalculating using recursive fibonacci. Please wait..."
+print "\tUsing recursion {}".format(recursive_fibo(11))
+recursive_time = time.clock() - startTime - iterrative_time
+print "\tRun time... %.5f(secs) or %.5f(mins)" % (recursive_time, (recursive_time / 60.0))
+"""
