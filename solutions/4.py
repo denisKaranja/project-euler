642e469a18061916f80ec7979ea66b4440f5e1b9
Denis Karanja <deebeat@students.uonbi.ac.ke>
2014-12-14 19:27:49 +0300

    Initial commit

---
+++ solutions/4.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+"""
+Author: Denis Karanja,
+Institution: The University of Nairobi, Kenya,
+Department: School of Computing and Informatics, Chiromo campus
+Email: dee.caranja@gmail.com,
+Task: 4(Find the largest palindrome made from the product of two 3-digit numbers.)
+License type: MIT :)
+Status: PENDING...
+"""
