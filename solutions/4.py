9ca3ccfa420598408e820a64331755ed83442e00
Denis Karanja <deebeat@students.uonbi.ac.ke>
2014-12-14 19:56:55 +0300

    Function to check if a number is a palindrome created

--- solutions/4.py
+++ solutions/4.py
@@ -8,3 +8,11 @@ Task: 4(Find the largest palindrome made from the product of two 3-digit numbers
 License type: MIT :)
 Status: PENDING...
 """
+def is_palindrome(num):
+	new_num = str(num)[::-1]
+	if int(new_num) == num:
+		return True
+	else:
+		return False
+
+is_palindrome(90099)
