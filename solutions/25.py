7942435119fc6b63b97ba2228d4dbf9df6c3c590
Denis Karanja <deebeat@students.uonbi.ac.ke>
2014-12-15 01:06:26 +0300

    Added iteratibe fibonacci function

--- solutions/25.py
+++ solutions/25.py
@@ -3,3 +3,16 @@
 #Department: School of Computing and Informatics,
 #Email: dee.caranja@gmail.com,
 #Euler project solution = 25(The first term in fibonacci to contain 1000digits)
+
+def iterative_fibo(num):
+	x = i = evenSum = 0
+	y = z = 1
+	limit = 4000000
+
+	for i in range(num+1):
+		x = y
+		y = z
+		z = x + y
+	return x
+
+print iterative_fibo(5)
