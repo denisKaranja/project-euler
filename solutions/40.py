6436151803192b2307e595b51ce04c50c1a199cf
Denis Karanja <deebeat@students.uonbi.ac.ke>
2014-12-15 08:13:25 +0300

    Problem 40 completely solved

---
+++ solutions/40.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+"""
+Author: Denis Karanja,
+Institution: The University of Nairobi, Kenya,
+Department: School of Computing and Informatics, Chiromo campus
+Email: dee.caranja@gmail.com,
+Task: 40(Champernowne's constant)
+License type: MIT :)
+Status: PENDING...
+"""
+import time
+startTime = time.clock()
+
+limit = 1000000
+product = j =1
+new_string = ''
+print("Calculating...")
+for i in range(1,limit+1):
+	new_string += str(i)
+
+for count in range(7):
+	product *= int(new_string[j-1])
+	j *= 10
+
+print product
+print "Run time...{} secs \n".format(round(time.clock() - startTime, 4))
