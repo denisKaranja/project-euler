2c80e199df29e93b7d09a879397677d386ce4099
Denis Karanja <deebeat@students.uonbi.ac.ke>
2014-12-22 21:39:47 +0300

    Problem 4 completely solved

--- solutions/4.py
+++ solutions/4.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 """
 Author: Denis Karanja,
 Institution: The University of Nairobi, Kenya,
@@ -6,31 +6,34 @@ Department: School of Computing and Informatics, Chiromo campus
 Email: dee.caranja@gmail.com,
 Task: 4(Find the largest palindrome made from the product of two 3-digit numbers.)
 License type: MIT :)
-Status: PENDING...
+Status: COMPLETED...
 """
+import time
+startTime = time.clock()
+
 def is_palindrome(num):
-	new_num = str(num)[::-1]#reverse string
-	if int(new_num) == num:
+	reversed_num = str(num)[::-1]#reverse string
+	if int(reversed_num) == num:
 		return True
 	else:
 		return False
 
-palindromes = []
+def multiple_palindrome(num_one = 100, num_two = 1000):
+	(palindromes, limit) = ([], 1000)
 
-def multiplier_3_dig(num_one, num_two):
-	limit = 999
-	for i in xrange(1, limit+1):
-		product = num_one * num_two
-		if is_palindrome(product):
-			palindromes.append(product)
-			num_one += 1
-			num_two += 1
-		else:
-			num_one += 1
-			num_two += 1
-	return palindromes
+	for i in range(limit + 1):
+		if num_two >= 100:
+			for j in range(limit +1):
+				product = num_one * num_two
+				if num_one < limit:
+					if is_palindrome(product):
+						palindromes.append(product)
+				num_one += 1
 
+		num_two -= 1
+		num_one = 100
+	return max(palindromes)
 
-
-
-print multiplier_3_dig(100, 100)
+print "Calculating largest 3 digit product palindrome..."
+print multiple_palindrome()
+print "Run time... {} secs\n".format(round((time.clock() - startTime), 4))
