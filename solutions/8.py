f0db6a57f7d1e4978567105c576e0f30544f3314
Denis Karanja <deebeat@students.uonbi.ac.ke>
2014-12-23 00:42:59 +0300

    Problem 8 completely solved

--- solutions/8.py
+++ solutions/8.py
@@ -7,13 +7,40 @@ Email: dee.caranja@gmail.com,
 Task: 8(Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. 
 	What is the value of this product?)
 License type: MIT :)
-Status: PENDING...
+Status: COMPLETED...
 """
+import time
+startTime = time.clock()
 
-file_handle = open("8_nums.txt", 'r')
-product = 0
-string = '123456'
-for line in file_handle:
-	first = line[0:13]
-	last = line[13:]
-	print len(line)
+(product, lim, file_handle, sum_, g_product) = (1, 13, open("8_nums.txt", 'r'), 0, [])
+
+def get_product(string):
+	product = 1
+	for i in string:
+		product *= int(i)
+	return product 
+
+def one_string():
+	string = ''
+	for line in file_handle:
+		line = line.strip()
+		string += line
+	return string
+
+def read_13_digit(start, stop):
+	string = one_string()
+
+	for i in string:
+		first = string[start:stop]
+
+		if len(first) == 13:
+			result = get_product(first)
+			g_product.append(result)
+			(start, stop) = (start + 1, stop + 1)
+			read_13_digit(start, stop)
+
+	return max(g_product)
+
+print "Calculating..."
+print(read_13_digit(0, 13))
+print "Run time... {} secs\n".format(round((time.clock() - startTime), 4))
