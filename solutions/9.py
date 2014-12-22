9e14c8472be6ae333fbd01689f1e111be87c94f3
Denis Karanja <deebeat@students.uonbi.ac.ke>
2014-12-23 00:58:02 +0300

    Initial commit(prob 9)

---
+++ solutions/9.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+"""
+Author: Denis Karanja,
+Institution: The University of Nairobi, Kenya,
+Department: School of Computing and Informatics, Chiromo campus
+Email: dee.caranja@gmail.com,
+Task: 9(Pythagorean triplet such that a^2 + b^2 = c^2 and a < b < c and a + b + c = 1000)
+License type: MIT :)
+Status: PENDING...
+"""
