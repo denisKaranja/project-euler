d1d8bd56d1238681d83e7dc1bf31d574a23d3b89
Denis Karanja <deebeat@students.uonbi.ac.ke>
2014-12-23 21:50:19 +0300

    Problem 16 revised to a faster one liner

--- solutions/16.py
+++ solutions/16.py
@@ -6,11 +6,4 @@ Email: dee.caranja@gmail.com,
 Euler project solution = 16(sum of digits in 2^1000 eg 2^4 = 16 there4 1+6=7)
 Status: COMPLETED...
 """
-import math
-digitSum = 0
-answer = str(int(math.pow(2, 1000)))
-
-for digit in answer:
-	digitSum += int(digit)
-
-print digitSum
+print sum([int(x) for x in str(int(2**1000))])
