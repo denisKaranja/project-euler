8eb08dab489b8b872cdde992a2cd7dbca4c137c5
Denis Karanja <deebeat@students.uonbi.ac.ke>
2014-12-25 23:24:19 +0300

    An iterative self_power method added in prob 48

--- solutions/48.py
+++ solutions/48.py
@@ -5,12 +5,18 @@
 #Euler project solution = 48(Sum of Self powers i.e 1^1 + 2^2 + 3^3 +...+ 100^100)
 import time
 
-def selfPower(num):
+def recursive_self_power(num):
 	if num == 1:
 		return 1
 	else:
 		return num**num + selfPower(num - 1)
+
+def iter_self_power(num, list_sum = 0):
+	for i in xrange(1, num+1):
+		list_sum += i**i
+	return list_sum
+
 startTime = time.clock()
-a = selfPower(999) + 1000**1000
+a = iter_self_power(1000)
 print str(a)[-10:]
 print "Program took %f secsonds to execute" % (time.clock() - startTime)
