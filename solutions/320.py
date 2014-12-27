bb13983b3fc9e2e9cf4fd171c9ef8633f61cb99c
Denis Karanja <deebeat@students.uonbi.ac.ke>
2014-12-27 21:17:42 +0300

    Initial commit for 320.py

---
+++ solutions/320.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+"""
+Author: Denis Karanja,
+Institution: The University of Nairobi, Kenya,
+Department: School of Computing and Informatics, Chiromo campus
+Email: dee.caranja@gmail.com,
+Task: 320(Factorials divisible by a huge int)
+License type: MIT :)
+Status: PENDING...
+"""
+import time
+
+def factorial(num):
+	"""An iterative fibonacci number finder"""
+	product = 1
+	for i in xrange(num):
+		product *= (i + 1)
+	return product
+
+start = time.clock()
+print factorial(5)
+print "Run time...{}(secs)".format(time.clock()-start)
