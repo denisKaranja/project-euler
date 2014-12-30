a349563d9c3a09dd0e55e2761ea0c3b3f214e751
Denis Karanja <deebeat@students.uonbi.ac.ke>
2014-12-30 21:05:09 +0300

    Problem 29 completely solved

---
+++ solutions/29.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+"""
+Author: Denis Karanja,
+Institution: The University of Nairobi, Kenya,
+Department: School of Computing and Informatics, Chiromo campus
+Email: dee.caranja@gmail.com,
+Task: 29(Distinct powers)
+License type: MIT :)
+Status: COMPLETED...
+"""
+def distinct_powers(limit):
+	"""Get the number of distinct powers"""
+	powers = []
+	for a in xrange(2, limit+1):
+		for b in xrange(2, limit+1):
+			powers.append(a**b)
+
+	return len(sorted(set(powers)))
+
+
+print distinct_powers(100)
