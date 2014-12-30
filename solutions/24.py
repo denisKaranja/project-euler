0f6ba21587f3ea5b55284a1a384b8dcfe0a48ade
Denis Karanja <deebeat@students.uonbi.ac.ke>
2014-12-31 02:43:18 +0300

    Problem 24 completely solved

---
+++ solutions/24.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+"""
+Author: Denis Karanja,
+Institution: The University of Nairobi, Kenya,
+Department: School of Computing and Informatics, Chiromo campus
+Email: dee.caranja@gmail.com,
+Task: 24(Lexicographic permutations)
+License type: MIT :)
+Status: COMPLETED...
+"""
+from itertools import permutations
+import time
+def generate(my_list):
+	solutions = []
+	for x in permutations(my_list):
+		solutions.append(x)
+
+	return sorted(solutions)[999999]
+
+a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+start = time.clock()
+print generate(a)
+print "Run time...{}(secs)".format(round(time.clock()- start, 5))
