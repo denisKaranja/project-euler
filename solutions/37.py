d392383fee00b2a044cca3b1c529354c39b00135
Denis Karanja <deebeat@students.uonbi.ac.ke>
2014-12-31 03:05:01 +0300

    Initial commit(37.py)

---
+++ solutions/37.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+"""
+Author: Denis Karanja,
+Institution: The University of Nairobi, Kenya,
+Department: School of Computing and Informatics, Chiromo campus
+Email: dee.caranja@gmail.com,
+Task: 27(Truncatable primes eg 3797->379->37->3 and 3797->797->97->7)
+License type: MIT :)
+Status: PENDING...
+"""
